82d885d658e4f6c35820e5e29ee06ab3ceee3dc5
robinlant <robinlant@proton.me>
2025-12-02 15:14:14 +0100

    feat: add env vars configuration

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.0
 
 require (
 	github.com/golang/snappy v1.0.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/robinlant/mywiki/internal/wiki/internal/store"
 	"github.com/robinlant/mywiki/internal/wiki/internal/web"
 )
 
-var addr = ":8800"
+type Config struct {
+	Addr     string
+	MongoURI string
+	DB       string
+}
 
-var mongoCon = "mongodb://127.0.0.1:27017"
-var db = "test"
+func LoadConfig() Config {
+	return Config{
+		Addr:     mustGetenv("ADDR"),
+		MongoURI: mustGetenv("MONGO_CON"),
+		DB:       mustGetenv("MONGO_DB"),
+	}
+}
+
+func mustGetenv(key string) string {
+	v := os.Getenv(key)
+	if v == "" {
+		panic(fmt.Errorf("failed to get env var '%s'", key))
+	}
+	return v
+}
 
 func main() {
-	st, disc := store.NewMongoStore(mongoCon, db)
+	if err := godotenv.Load(); err != nil {
+		log.Println("[WARN] No .env file found, using system env")
+	}
+	conf := LoadConfig()
+
+	st, disc := store.NewMongoStore(conf.MongoURI, conf.DB)
 	defer disc()
-	log.Printf("[INFO] Starting a web server at %s", addr)
-	web.Run(st, addr)
+	log.Printf("[INFO] Starting a web server at %s", conf.Addr)
+	web.Run(st, conf.Addr)
 }
